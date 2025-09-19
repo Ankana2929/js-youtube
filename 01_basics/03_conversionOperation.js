396097ee8813c628679b052dae1c9b72aa7e7611
Ankana Jana <janaankana9@gmail.com>
2025-09-19 14:00:33 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = true
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = "Anku"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 99
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
