cd1af80e7611fca56ca8a48314fed22b4af65c55
Ankana Jana <janaankana9@gmail.com>
2025-10-06 16:05:36 +0000

    Datatype summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,5 +14,31 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 99
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+//console.log(stringNumber)
+//console.log(typeof stringNumber);
+
+// ******************** Operations ****************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "Hello"
+let str2 =" Ankana"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2 );
+// console.log(1 + "2" );
+// console.log(1 + 2 + "2" );
+
+// console.log(true);
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+
+let x = 3
+const y = ++x
+console.log(x);
+console.log(y);
