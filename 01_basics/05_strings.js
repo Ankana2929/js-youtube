812455bec742c862247227010c89095e93fe575d
Ankana Jana <janaankana9@gmail.com>
2025-10-08 17:09:24 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,21 @@
+const name = "Ankana"
+const repoCount = 5
+// console.log(name + repoCount);
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName = new String('Ankana-jana')
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('n'));
+const newString = gameName.substring(0,4)
+console.log(newString);
+const anotherString = gameName.slice(-6, 4)
+console.log(anotherString);
+
+
+
+
+
+
