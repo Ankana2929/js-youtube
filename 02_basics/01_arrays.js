4a15b842159383909d92f5e34c76c51cd3677668
Ankana Jana <janaankana9@gmail.com>
2025-10-17 16:26:46 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,38 @@
+// Arrays
+
+const myArray = [0, 1, 2, 3, 4, 5]
+
+const myHeros = ["Ironman", "CaptainAmerica", "Thor", "Hulk"]
+const myNewArr = new Array(1, 2, 3, 4, 5, 6)
+// console.log(myArray[1]);
+// ++++++++++++++++++++++++++Array Methods+++++++++++++++++++
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(9)
+// myArray.shift()
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(3));
+// const newArray = myArray.join()
+// console.log(myArray);
+// console.log(newArray);
+// console.log(typeof newArray);
+
+// +++++++++++++++++++Slice and Splice+++++++++++++++++++++++
+
+console.log("A", myArray);
+const mny1 = myArray.slice(1, 4)
+console.log("B", myArray);
+console.log(mny1);
+
+const mny2 = myArray.splice(1, 4)
+console.log("C", myArray);
+console.log(mny2);
+
+
+
+
+
